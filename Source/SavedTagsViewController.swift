bf6c2719e7f57a154b758e076781aefff92cffc9
KristynaKohoutova <83544654+KristynaKohoutova@users.noreply.github.com>
2021-05-02 16:40:29 +0200

    Add files via upload

---
+++ Source/SavedTagsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SavedTagsViewController.swift
+//  NURAPI Swift Demo
+//
+
+import Foundation
+import UIKit
+
+public class SavedTagsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, BluetoothDelegate, UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
